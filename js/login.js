ddab27e39a6fff5abf4df26a9738b4134d1bb550
Florencia Eusebi <eusebiflorencia@gmail.com>
2021-04-04 23:25:39 -0300

    BQA-09: javascript

---
+++ js/login.js
@@ -0,0 +1,21 @@
+/* (document).ready(function () {
+  (".myButton").click(function (){
+    var email = (".myEmail").val();
+    var fullName = (".myFullName").val();
+    var password = (".myPassword").val();
+    var confirmPassword = (".myConfirmPassword").val();
+
+      if (email ==""){
+       (".Errors").html ("Email is required");
+      (".errorDiv").css ("Display","Block");
+        alert ("no");
+        return false;
+      }
+      if (fullName ==""){
+       (".Errors").html ("Full name is required");
+       (".errorDiv").css ("Display","Block");
+        alert ("no2");
+        return false;
+      }
+})
+}); */
---
+++ js/register.js
@@ -0,0 +1,36 @@
+function validateRegisterForm(){
+  var email = document.myRegisterForm.email.value;
+  var fullName = document.myRegisterForm.fullName.value;
+  var password = document.myRegisterForm.password.value;
+  var confirmPassword = document.myRegisterForm.confirmPassword.value;
+  if (email == null || email ==""){
+    Error1.innerHTML = "Email is required";
+    return false;
+  } else {
+    Error1.innerHTML = "";}
+  if (fullName == null || fullName ==""){
+    Error2.innerHTML = "Full name is required";
+    return false;
+  } else {
+    Error2.innerHTML = "";}
+  if (password == null || password ==""){
+    Error3.innerHTML = "Password is required";
+    return false;
+  } else {
+    Error3.innerHTML = "";}
+  if (confirmPassword == null || confirmPassword ==""){
+    Error4.innerHTML = "Please confirm password";
+    return false;
+  } else {
+    Error4.innerHTML = "";}
+  if (confirmPassword != password){
+    Error5.innerHTML = "Password does not match";
+    return false;
+  } else {
+    Error5.innerHTML = "";}
+  if (confirmPassword = password || password != null || fullName != null || email != null || confirmPassword!= null){
+    Success.innerHTML = "Success";
+    return false;
+  } else {
+    Success.innerHTML = "";}
+}
